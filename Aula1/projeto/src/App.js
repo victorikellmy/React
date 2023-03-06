00dfadb2ac88ecba1cb6e93bb4d3fe1ccb8409ae
victorikellmy <victorikellmyferreira123@gmail.com>
2023-03-06 15:26:54 -0300

    Mudança de estrutura

--- Aula1/projeto/src/App.js
+++ Aula1/projeto/src/App.js
@@ -1,52 +1,37 @@
-const BemVindo = (props)=>{
-  return(
-   <div>
-      <Sobre nome={props.nome} idade={props.idade} funcao={props.funcao}/>
-      <Social fb={props.facebook}/>
-  </div>
-  );
+import React, {Component} from "react";
+
+
+class Equipe extends Component{
+  render(){
+    return(
+      <div>
+        <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.cargo}/>  
+      </div>
+
+    );
+  }
 }
 
-
-const Sobre = (props) =>{
-  return(
-    <div>
-    <h3>Nome:{props.nome}</h3>
-    <h3>Idade:{props.idade} anos</h3>
-    <h3>Função:{props.funcao}</h3>
-    <br/>
-  </div>
-  );
-}
-
-const Social = (props) =>{
-  return(
-    <div>
-      <a>Social: </a>
-      <a href={props.fb}>Facebook </a>
-      <a>Instagram </a>
-      <a>YouTube </a>
-    </div>
-  );
-
+class Sobre extends Component{
+  render(){
+    return(
+      <div>
+        <h2>Nome:{this.props.nome}</h2>
+        <h2>Cargo:{this.props.cargo}</h2>
+        <h2>idade:{this.props.idade}</h2>
+        <hr/>
+      </div>
+    );
+  }
 }
 
 export default function App(){
-
   return(
-
     <div>
-      <h1>Conhecendo equipe</h1>
-      <BemVindo nome="Lucas" idade= "18" funcao ="Desenvolvedor" 
-      facebook="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.infoescola.com%2Finternet%2Fhistoria-do-facebook%2F&psig=AOvVaw1VZbEfw9qm_tIOOXekPTIR&ust=1678209596717000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPDz8ojox_0CFQAAAAAdAAAAABAE"/>
-      <BemVindo nome="Lana" idade= "19" funcao ="Back end" 
-      facebook="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.infoescola.com%2Finternet%2Fhistoria-do-facebook%2F&psig=AOvVaw1VZbEfw9qm_tIOOXekPTIR&ust=1678209596717000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPDz8ojox_0CFQAAAAAdAAAAABAE"/>
-      <BemVindo nome="Katarina" idade= "18" funcao ="Infraestrutura" 
-      facebook="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.infoescola.com%2Finternet%2Fhistoria-do-facebook%2F&psig=AOvVaw1VZbEfw9qm_tIOOXekPTIR&ust=1678209596717000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPDz8ojox_0CFQAAAAAdAAAAABAE"/>
-      <BemVindo nome="Victor" idade= "20" funcao ="Redes" 
-      facebook="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.infoescola.com%2Finternet%2Fhistoria-do-facebook%2F&psig=AOvVaw1VZbEfw9qm_tIOOXekPTIR&ust=1678209596717000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPDz8ojox_0CFQAAAAAdAAAAABAE"/>
-
+      <h1>Conheça nossa equipe</h1>
+      <Equipe nome="Joao" cargo="Programador" idade="18"/>
+      <Equipe nome="Lucas" cargo="Programador" idade="19"/>
+      <Equipe nome="Pedro" cargo="Programador" idade="20"/>
     </div>
-
   );
 }
